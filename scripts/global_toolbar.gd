b9ca7000a1774224f036807beac8973bb8a521b9
Benie Gouli <gouliaudrey@gmail.com>
2025-06-04 12:10:55 +0200

    started making the basics of the toolbar

---
+++ scripts/global_toolbar.gd
@@ -0,0 +1,23 @@
+extends Node
+signal toolbar_updated
+
+@export var max_size: int = 5
+@onready var slot_size: int # the expression will define the number of gates added
+var contents: Array = [] # 
+
+func add_gate() ->void:
+	toolbar_updated.emit()
+	
+func remove_gate() -> void:
+	toolbar_updated.emit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+	#$GridContainer.columns = slot_size
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
