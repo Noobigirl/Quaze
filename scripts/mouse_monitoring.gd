72da7800468e667faf879e046693c6e206f5f3c9
Benie Gouli <gouliaudrey@gmail.com>
2025-06-23 01:34:45 +0000

    Showing and hiding seems to work. Still investigating some bugs

--- scripts/mouse_monitoring.gd
+++ scripts/mouse_monitoring.gd
@@ -2,6 +2,7 @@ class_name MouseMonitoring
 extends Node2D
 
 @export var detection_zone: Rect2 
+var mouse_was_inside: bool = false
 
 func _process(delta: float) -> void:
 	# make use to only check if the mouse is within the viewport of the game
@@ -10,9 +11,12 @@ func _process(delta: float) -> void:
 
 func detecting_mouse():
 	var mouse_pos = get_global_mouse_position()
-	if detection_zone.has_point(mouse_pos) and !Toolbar.is_visible: # emiting the signal only if the mouse is in the detection zone and the toolbar is hidden
+	var mouse_is_inside = detection_zone.has_point(mouse_pos)
+	if mouse_is_inside and !mouse_was_inside: # emiting the signal only if the mouse is in the detection zone and the toolbar is hidden
 		#print("mouse detected")
 		Toolbar.play_animation.emit("showing")
-	elif Toolbar.is_visible: # emitting the signal only if the toolbar is visible
+		mouse_was_inside = true
+	elif !mouse_is_inside and mouse_was_inside: # emitting the signal only if the toolbar is visible
 		Toolbar.play_animation.emit("hiding")
+		mouse_was_inside = false
 	
